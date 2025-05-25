e521264d0295619da2b0dc4ac716dfcb03017352
lenam15924 <635104c026@st.utc2.edu.vn>
2025-05-25 23:13:22 +0700

    Update SanPham.h

--- SanPham.h
+++ SanPham.h
@@ -14,24 +14,21 @@ public:
     SanPham(const std::string& ma, const std::string& ten, double g) : QuanLy(ma), tenSP(ten), gia(g) {}
     ~SanPham() override {}
 
-    // Getters
     std::string getTenSP() const { return tenSP; }
     double getGia() const { return gia; }
 
-    // Setters
     void setTenSP(const std::string& ten) { tenSP = ten; }
     void setGia(double g) { gia = g; }
 
-    // Phương thức ảo (đa hình)
     void hienThiThongTin() const override;
 
-    // Thay đổi giá
+
     void thayDoiGia(double phanTramThayDoi);
 
-    // Quá tải toán tử
+
     bool operator==(const SanPham& other) const {
         return id == other.id;
     }
 };
 
-#endif // SANPHAM_H
+#endif 
